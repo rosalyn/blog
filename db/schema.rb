96acede7aca2d8e1606ee826d2aaddeaaf4e09db
Rosalyn Ku <rosalyn@Rosalyns-MacBook-Pro.local>
2015-08-19 21:57:38 -0700

    Reseting the db...

--- db/schema.rb
+++ db/schema.rb
@@ -31,15 +31,6 @@ ActiveRecord::Schema.define(version: 20150820040047) do
 
   add_index "comments", ["article_id"], name: "index_comments_on_article_id"
 
-  create_table "tags", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "article_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "tags", ["article_id"], name: "index_tags_on_article_id"
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
